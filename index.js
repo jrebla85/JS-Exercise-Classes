f5485f99f9dcff9170c192f1b0c2bd9615de9e06
jeremy black <jrebla85@yahoo.com>
2021-01-20 22:54:15 -0600

    Finished task 1

--- index.js
+++ index.js
@@ -42,8 +42,28 @@ class Airplane {
   */
   
  class Person {
+    constructor(name, age){
+      this.name = name;
+      this.age = age;
+      this.stomach = [];
+    };
     
-  }
+    eat(someFood){
+      if(this.stomach.length < 10){
+        this.stomach.push(someFood);
+      };
+    };
+
+    poop(){
+      while(this.stomach.length > 0){
+        this.stomach.pop();
+      };
+    };
+
+    toString(){
+      return `${this.name}, ${this.age}`;
+    };
+  };
   
   /*
     TASK 2
